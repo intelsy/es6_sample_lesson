b05a556c27c7dde4f3e838b583ec7a8d99fb02b8
intelsy <intel.sy.408@gmail.com>
2018-10-15 16:08:33 +0900

    Initiate

--- dist/script.js
+++ dist/script.js
@@ -23,7 +23,6 @@ var drinkChoices = _menuList.drinks.map(function (drink) {
 var questions = [{ type: 'confirm', name: 'place', message: 'For Here?', default: true }, { type: 'list', name: 'food', message: 'Select Food', choices: foodChoices }, { type: 'list', name: 'drink', message: 'Select Drink', choices: drinkChoices }];
 
 _inquirer2.default.prompt(questions).then(function (answers) {
-  console.log(answers);
   var place = answers.place,
       food = answers.food,
       drink = answers.drink;
